27d0ad0667f5df5af18de2596d79ac4a1705d533
GT <788449+gtgt@users.noreply.github.com>
2020-12-02 16:10:47 +0100

    style

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -20,3 +20,16 @@ $settings['file_private_path'] = $app_root . '/../private';
 $settings['trusted_host_patterns'] = [
   '^gmwe\.hu(\.lo)?$',
 ];
+
+if (substr($_SERVER['HTTP_HOST'], -3) === '.lo') {
+  $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+
+  $settings['cache']['bins']['render'] = 'cache.backend.null';
+  $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+  $settings['cache']['bins']['page'] = 'cache.backend.null';
+
+  $settings['extension_discovery_scan_tests'] = FALSE;
+
+  $config['system.performance']['css']['preprocess'] = FALSE;
+  $config['system.performance']['js']['preprocess'] = FALSE;
+}
